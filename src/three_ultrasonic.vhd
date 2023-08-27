2d8a236f09be9ba9c03764aca2c5114f7501e877
Usman Abdulsalam <usmanabdulsalamolanrewaju@gmail.com>
2023-08-27 03:19:09 +0100

    was able to get make the code to get distnce from three_ultrasonic.vhd work. next step is to use it as a component in vga_display.vhd

--- src/three_ultrasonic.vhd
+++ src/three_ultrasonic.vhd
@@ -13,10 +13,19 @@ ARCHITECTURE behaviour OF three_ultrasonic IS
             clk : IN STD_LOGIC;
             pulse : IN STD_LOGIC; -- echo
             triggerOut : OUT STD_LOGIC; -- trigger out
-            obstacle : OUT STD_LOGIC);
+            distanceOut : OUT INTEGER RANGE 0 TO 400);
     END COMPONENT;
+
+    SIGNAL ultrasonic_out_rear_left : INTEGER RANGE 0 TO 400; -- Signal to capture ultrasonic output1
+    SIGNAL ultrasonic_out_rear_center : INTEGER RANGE 0 TO 400; -- Signal to capture ultrasonic output2
+    SIGNAL ultrasonic_out_rear_right : INTEGER RANGE 0 TO 400; -- Signal to capture ultrasonic output3
 BEGIN
-    ultrasonic_Left : ultrasonic PORT MAP(clk, pulse(0), triggerOut(0), ultrasonic_out(0));
-    ultrasonic_Middle : ultrasonic PORT MAP(clk, pulse(1), triggerOut(1), ultrasonic_out(1));
-    ultrasonic_Right : ultrasonic PORT MAP(clk, pulse(2), triggerOut(2), ultrasonic_out(2));
+
+    ultrasonic_Left : ultrasonic PORT MAP(clk, pulse(0), triggerOut(0), ultrasonic_out_rear_left);
+    ultrasonic_Middle : ultrasonic PORT MAP(clk, pulse(1), triggerOut(1), ultrasonic_out_rear_center);
+    ultrasonic_Right : ultrasonic PORT MAP(clk, pulse(2), triggerOut(2), ultrasonic_out_rear_right);
+
+    rear_left <= ultrasonic_out_rear_left;
+    rear_center <= ultrasonic_out_rear_center;
+    rear_right1 <= ultrasonic_out_rear_right;
 END ARCHITECTURE;
